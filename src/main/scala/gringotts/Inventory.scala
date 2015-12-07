787fdf5ece36a7d609c235dbecf3bf907a4377cd
yuhuan <jyuhuan@gmail.com>
2015-12-07 11:28:29 -0500

    corpus file structure change

--- src/main/scala/gringotts/Inventory.scala
+++ src/main/scala/gringotts/Inventory.scala
@@ -4,7 +4,7 @@ package gringotts
   * @author Yuhuan Jiang (jyuhuan@gmail.com).
   */
 object Inventory {
-  val DeverbalNouns = this.getClass.getResourceAsStream("/list/deverbal-nouns.txt")
-  val ChoiConnotation = this.getClass.getResourceAsStream("/lexicon/choi-connotation.csv")
-  val RiloffSubjClue = this.getClass.getResourceAsStream("/lexicon/riloff-subjclue.tff")
+  val DeverbalNouns = this.getClass.getResourceAsStream("/list/deverbal-nouns/deverbal-nouns.txt")
+  val ChoiConnotation = this.getClass.getResourceAsStream("/lexicon/choi-connotation/choi-connotation.csv")
+  val RiloffSubjClue = this.getClass.getResourceAsStream("/lexicon/riloff-subjclue/riloff-subjclue.tff")
 }
