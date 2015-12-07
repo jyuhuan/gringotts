134af7bdb29115ad6865f4330d0888d4e19f6eb1
yuhuan <jyuhuan@gmail.com>
2015-12-07 17:49:16 -0500

    structurized

--- src/main/scala/me/yuhuan/gringotts/Inventory.scala
+++ src/main/scala/me/yuhuan/gringotts/Inventory.scala
@@ -4,7 +4,15 @@ package me.yuhuan.gringotts
   * @author Yuhuan Jiang (jyuhuan@gmail.com).
   */
 object Inventory {
-  val DeverbalNouns = this.getClass.getResourceAsStream("/list/deverbal-nouns/deverbal-nouns.txt")
-  val ChoiConnotation = this.getClass.getResourceAsStream("/lexicon/choi-connotation/choi-connotation.csv")
-  val RiloffSubjClue = this.getClass.getResourceAsStream("/lexicon/riloff-subjclue/riloff-subjclue.tff")
+
+  object Lexicon {
+    val ChoiConnotation = this.getClass.getResourceAsStream("/lexicon/choi-connotation/choi-connotation.csv")
+    val RiloffSubjClue = this.getClass.getResourceAsStream("/lexicon/riloff-subjclue/riloff-subjclue.tff")
+    val GeneralInquireGfbf = this.getClass.getResourceAsStream("/lexicon/general-inquire/general-inquire-gfbf.txt")
+  }
+
+  object List {
+    val DeverbalNouns = this.getClass.getResourceAsStream("/list/deverbal-nouns/deverbal-nouns.txt")
+  }
+
 }
