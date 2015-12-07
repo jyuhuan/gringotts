60fc004ec1678b3e083d6b6c196c93c718a9c428
yuhuan <jyuhuan@gmail.com>
2015-12-07 01:43:09 -0500

    package move

--- src/main/scala/gringotts/list/DeverbalNouns.scala
+++ src/main/scala/gringotts/list/DeverbalNouns.scala
@@ -1,14 +1,22 @@
-package gringotts
+package gringotts.list
 
+import gringotts.Inventory
 import me.yuhuan.util.io._
 
 
 /**
   * Deverbal nouns provided by NomBank.
+  *
   * @author Yuhuan Jiang (jyuhuan@gmail.com).
   */
 object DeverbalNouns extends (String ⇒ Boolean) {
   private[gringotts] val deverbalNouns = TextFile.readLines(Inventory.DeverbalNouns).toSet
   def apply(word: String) = this contains word
   def contains(word: String) = deverbalNouns contains word
+}
+
+object DeverbalNounsTest extends App {
+
+  assert(DeverbalNouns contains "pollution")
+
 }
