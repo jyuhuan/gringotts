baa8692e70ae944db8eadbd62c1ef0256cf5d449
yuhuan <jyuhuan@gmail.com>
2015-12-08 21:00:44 -0500

    +

--- src/main/scala/me/yuhuan/gringotts/Inventory.scala
+++ src/main/scala/me/yuhuan/gringotts/Inventory.scala
@@ -9,6 +9,10 @@ object Inventory {
     val ChoiConnotation = this.getClass.getResourceAsStream("/lexicon/choi-connotation/choi-connotation.csv")
     val RiloffSubjClue = this.getClass.getResourceAsStream("/lexicon/riloff-subjclue/riloff-subjclue.tff")
     val GeneralInquireGfbf = this.getClass.getResourceAsStream("/lexicon/general-inquire/general-inquire-gfbf.txt")
+    object ChoiGoodForBadFor {
+      val GoodFor = this.getClass.getResourceAsStream("/lexicon/choi-goodfor-badfor/goodfor.csv")
+      val BadFor = this.getClass.getResourceAsStream("/lexicon/choi-goodfor-badfor/badfor.csv")
+    }
   }
 
   object List {
---
+++ src/main/scala/me/yuhuan/gringotts/lexicon/ChoiGoodForBadFor.scala
@@ -0,0 +1,48 @@
+package me.yuhuan.gringotts.lexicon
+
+import me.yuhuan.gringotts.Inventory
+import me.yuhuan.util.io.TextFile
+
+/**
+  * GoodFor and BadFor collected by Yoonjung Choi.
+  * @author Yuhuan Jiang (jyuhuan@gmail.com).
+  */
+object ChoiGoodForBadFor {
+  private val data: Map[String, Map[String, String]] = {
+    val goodFors = TextFile.readLines(Inventory.Lexicon.ChoiGoodForBadFor.GoodFor).map { l =>
+      val parts = l.split(',').map(_.tail.init)
+      val word = parts(0)
+      val pos = parts(1)
+      //val frameNetId = parts(2)
+      (word, pos, "goodFor")
+    }
+
+    val badFors = TextFile.readLines(Inventory.Lexicon.ChoiGoodForBadFor.BadFor).map { l =>
+      val parts = l.split(',')
+      val word = parts(0)
+      val pos = parts(1)
+      //val frameNetId = parts(2)
+      (word, pos, "badFor")
+    }
+
+    (goodFors ++ badFors).groupBy(_._1).map { case (word, ts2) =>
+      word -> ts2.map { case (_, pos, pol) =>
+        pos -> pol
+      }.toMap
+    }
+  }
+
+  def apply(word: String, partOfSpeech: String): String = data(word)(partOfSpeech)
+  def apply(word: String): Map[String, String] = data(word)
+
+  def contains(word: String, partOfSpeech: String): Boolean = data.contains(word) && data(word).contains(partOfSpeech)
+  def contains(word: String): Boolean = data contains word
+}
+
+private object ChoiGoodForBadForTest extends App {
+
+  val result = ChoiGoodForBadFor("pity")("v")
+  assert(result == "goodFor")
+
+  val bp = 0
+}
